d9ab42a571e160ef4a7f3697ef83a673e330fc7e
master-abhay <master.abhay71052@gmail.com>
2024-02-21 17:01:36 +0530

    completed home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -35,7 +36,8 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child: Column
+          (
           children: [
             Container(
               padding: const EdgeInsets.fromLTRB(20, 5, 10, 5),
@@ -109,7 +111,7 @@ class _HomeState extends State<Home> {
                           padding: const EdgeInsets.all(5),
                           margin: const EdgeInsets.all(2),
                           decoration: BoxDecoration(
-                              color: const Color(0xffff14900),
+                              color: Colors.purple,
                               borderRadius: BorderRadius.circular(5)),
                           child: Text(
                             navBarItem[index],
@@ -124,9 +126,9 @@ class _HomeState extends State<Home> {
 //Creating the CarouselSlider
             CarouselSlider(
               options: CarouselOptions(
-                height: 200.0,
+                height: 180.0,
                 aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
+                viewportFraction: 0.7,
                 initialPage: 0,
                 enableInfiniteScroll: true,
                 reverse: false,
@@ -141,11 +143,50 @@ class _HomeState extends State<Home> {
               items: items.map((item) {
                 return Builder(builder: (BuildContext context) {
                   return InkWell(
-                    onTap: () {
-                      print("Current Color is $item");
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(color: item),
+                    onTap: () {},
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Card(
+                        child: Stack(
+                          children: [
+                            Image.asset(
+                              "images/card_image.jpg",
+                              height: double.infinity,
+                              width: double.infinity,
+                              fit: BoxFit.fill,
+                            ),
+                            Positioned(
+                              top: 130,
+                              left: 10,
+                              child: Container(
+                                height: MediaQuery.of(context).size.height,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [
+                                  Colors.black12.withOpacity(0),
+                                  Colors.black12.withOpacity(1)
+                                ])),
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "NEWS Headline",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      ),
+                                      Text(
+                                        "Description........",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 10),
+                                      )
+                                    ]),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   );
                 });
@@ -154,31 +195,83 @@ class _HomeState extends State<Home> {
 
             //Creating the Cards with scrollable listView
 
+            //Adding Text
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  Text("Latest News",style: textStyle,),],
+              ),
+            ),
             ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {},
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
                     child: ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
-                      clipBehavior: Clip.hardEdge,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(20.0),
+                        bottomRight: Radius.circular(30.0),
+                        bottomLeft: Radius.circular(40.0),
+                      ),
                       child: Card(
-                          elevation: 30,
-                          shadowColor: Colors.amber,
+                          elevation: 15,
+                          shadowColor: Colors.purpleAccent,
                           child: Stack(children: [
-                            Container(
-                              decoration:
-                                  const BoxDecoration(color: Colors.green),
-                              height: MediaQuery.of(context).size.height - 550,
-                              width: MediaQuery.of(context).size.width,
-                              child: Text("This is Card"),
-                            ),
+                            Image.asset("images/card_image.jpg"),
+                            Positioned(
+                                top: 220,
+                                right: 0,
+                                bottom: 0,
+                                left: 20,
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        gradient: LinearGradient(colors: [
+                                      Colors.black12.withOpacity(0),
+                                      Colors.black.withOpacity(1)
+                                    ])),
+                                    child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text("News Headline",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold)),
+                                          SizedBox(
+                                            width: 6,
+                                          ),
+                                          Text("News Description",
+                                              style: TextStyle(
+                                                  color: Colors.white))
+                                        ])))
                           ])),
                     ),
-                  );
-                })
+                  ),
+                );
+              },
+            ),
+
+            // Creting Show More Button
+            ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: () {},
+              child: Text(
+                'show more',
+                style: textStyle,
+              ),
+            )
           ],
         ),
       ),
@@ -187,4 +280,19 @@ class _HomeState extends State<Home> {
 
   // list for the Carousel slider:
   final List items = [Colors.amber, Colors.green, Colors.blueAccent];
+
+  //Creating the Button Style
+  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+    foregroundColor: Colors.blue,
+    backgroundColor: Colors.grey[300],
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(2)),
+    ),
+  );
+
+  //Creating the TextStyle
+  final TextStyle textStyle = const TextStyle(
+      color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 20.0);
 }
